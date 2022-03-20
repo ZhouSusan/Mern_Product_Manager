78c9538e963c8d31244652ea044a61baddbf392f
Susan <szhou089@gmail.com>
2022-03-20 11:35:39 -0700

    "initial commit"

---
+++ client/src/views/Main.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react'
+import ProductForm from '../components/ProductForm';
+import axios from 'axios';
+
+export default () => {
+    
+    return (
+        <div>
+            <ProductForm />
+        </div>
+    )
+}
